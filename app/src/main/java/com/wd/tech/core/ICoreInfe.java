39dad5ca3e49d8023e0668f1282b2b00b1f7f2e3
xhw15815912 <1076731817@qq.com>
2019-02-18 20:48:46 +0800

    11

--- app/src/main/java/com/wd/tech/core/ICoreInfe.java
+++ app/src/main/java/com/wd/tech/core/ICoreInfe.java
@@ -1,8 +1,12 @@
 package com.wd.tech.core;
 
+import com.wd.tech.core.exception.ApiException;
+
 /**
  * 作者：古祥坤 on 2019/2/18 15:22
  * 邮箱：1724959985@qq.com
  */
-public interface ICoreInfe {
+public interface ICoreInfe<T> {
+    void success(T data);
+    void fail(ApiException e);
 }
