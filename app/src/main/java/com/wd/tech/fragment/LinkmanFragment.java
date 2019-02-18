3895b726d97a4d763dd57380343ebbe440520d7b
bulangxiong <45414734+bulangxiong@users.noreply.github.com>
2019-02-18 20:53:00 +0800

    消息

---
+++ app/src/main/java/com/wd/tech/fragment/LinkmanFragment.java
@@ -0,0 +1,26 @@
+package com.wd.tech.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wd.tech.R;
+
+/**
+ * Created by zxk
+ * on 2019/2/18 19:47
+ * QQ:666666
+ * Describe:
+ */
+public class LinkmanFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.linkman_layout, container, false);
+        return inflate;
+    }
+}
