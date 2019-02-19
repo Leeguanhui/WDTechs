c0cd0d2ad80242f067ebaf33efec06ced559dc9e
AndroidGxk <44991107+AndroidGxk@users.noreply.github.com>
2019-02-19 21:09:22 +0800

    侧滑

---
+++ app/src/main/java/com/wd/tech/activity/secondactivity/TaskActivity.java
@@ -0,0 +1,22 @@
+package com.wd.tech.activity.secondactivity;
+
+import com.wd.tech.R;
+import com.wd.tech.core.WDActivity;
+
+public class TaskActivity extends WDActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_task;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
--- app/src/main/java/com/wd/tech/core/AllUrls.java
+++ app/src/main/java/com/wd/tech/core/AllUrls.java
@@ -1,13 +1,17 @@
 package com.wd.tech.core;
 
+import com.wd.tech.bean.LoginUserInfoBean;
 import com.wd.tech.bean.NewsBannder;
 import com.wd.tech.bean.Result;
 
 import java.util.List;
 
 import io.reactivex.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -21,4 +25,11 @@ public interface AllUrls {
     @GET("information/v1/bannerShow")
     Observable<Result<List<NewsBannder>>> Bannder(
     );
+
+    //用户登录
+    @FormUrlEncoded
+    @POST("user/v1/login")
+    Observable<Result<LoginUserInfoBean>> login(
+            @Field("phone") String phone,
+            @Field("pwd") String pwd);
 }
